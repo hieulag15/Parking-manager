bb4f8228eca9a6e9e46a9a0fe52e29e4b5099526
hieulag15 <hieuthanhtran12@gmail.com>
2024-10-04 15:23:31 +0700

    config mongoose

---
+++ backend/src/config/mongoose.js
@@ -0,0 +1,15 @@
+// Assuming this is in mongoose.js
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    // Remove the useCreateIndex option from this call
+    await mongoose.connect('mongodb+srv://hieuthanhtran12:LsTCYoP3gQBYGQct@cluster0.j0vyd.mongodb.net/myShop?retryWrites=true&w=majority&appName=Cluster0');
+    console.log('MongoDB Connected...');
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;

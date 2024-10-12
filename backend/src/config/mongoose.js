7f883429d033b184ef962b5310aabdb6ce2a618f
hieulag15 <hieuthanhtran12@gmail.com>
2024-10-12 15:41:05 +0700

    create Driver

--- backend/src/config/mongoose.js
+++ backend/src/config/mongoose.js
@@ -1,10 +1,11 @@
 // Assuming this is in mongoose.js
 import mongoose from 'mongoose';
+import { env } from './enviroment.js';
 
 const connectDB = async () => {
   try {
     // Remove the useCreateIndex option from this call
-    await mongoose.connect('mongodb+srv://hieuthanhtran12:LsTCYoP3gQBYGQct@cluster0.j0vyd.mongodb.net/ParkingLot?retryWrites=true&w=majority&appName=Cluster0');
+    await mongoose.connect(env.MONGODB_URI);
     console.log('MongoDB Connected...');
   } catch (error) {
     console.error(`Error: ${error.message}`);

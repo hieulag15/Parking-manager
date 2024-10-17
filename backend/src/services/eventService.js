bf5eb419fd54d8976df5171feb468b4c777cb581
BaoNun1205 <94816323+BaoNun1205@users.noreply.github.com>
2024-10-18 02:20:30 +0700

    fix event

--- backend/src/services/eventService.js
+++ backend/src/services/eventService.js
@@ -11,21 +11,20 @@ const create = async (eventData) => {
 
 const findEvent = async ({ pageSize, pageIndex, ...params }) => {
     try {
-        // Tạo bộ lọc từ params
         const filter = {};
         if (params.name) filter['name'] = params.name;
         if (params.position) filter['position'] = params.position;
         if (params.licensePlate) filter['licensePlate'] = params.licensePlate;
         if (params.driverName) filter['driverName'] = params.driverName;
     
-        // Tìm kiếm và phân trang
         const totalCount = await Event.countDocuments(filter);
         const totalPage = Math.ceil(totalCount / pageSize);
         const events = await Event.find(filter)
           .sort({ createdAt: -1 })
           .skip((pageIndex - 1) * pageSize)
           .limit(pageSize)
-          .populate('driverId');
+          .populate('parkingTurnId')
+          .populate('driverId')
     
         return {
           data: events,

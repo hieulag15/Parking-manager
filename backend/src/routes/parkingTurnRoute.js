e197c136170f7e150c9b9f4825e9ad584bf5eea3
HieuTT <hieuthanhtran12@gmail.com>
2024-11-20 23:52:44 +0700

    find Slot

--- backend/src/routes/parkingTurnRoute.js
+++ backend/src/routes/parkingTurnRoute.js
@@ -5,5 +5,6 @@ import { verifyTokenMidleware } from '../middlewares/verifytokenMidleware.js';
 const router = express.Router();
 
 router.post('/', verifyTokenMidleware.verifyTokenAndAdmin, parkingTurnController.createParkingTurn);
+router.get('/find-slot/:_parking_id', verifyTokenMidleware.verifyTokenAndAdmin, parkingTurnController.findEmptySlot);
 
 export const parkingTurnRoute = router;

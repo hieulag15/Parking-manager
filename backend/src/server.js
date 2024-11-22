f0981ff35a05b1023f32b2da8aac36fb10c1d303
BaoNun1205 <94816323+BaoNun1205@users.noreply.github.com>
2024-11-22 13:34:34 +0700

    socket

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ const httpServer = createServer(app);
 io = new Server(httpServer, {
   path: '/socket',
   cors: {
-    origin: ['http://localhost:5173/'],
+    origin: [`https://${env.APP_HOST}:5173/`],
   },
   transports: ['websocket'],
 });
